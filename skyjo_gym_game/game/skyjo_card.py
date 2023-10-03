a2b125ab8545a0c70c08fd31c4f69ab31c95d518
Grant Lewis <eagleflyer2500@gmail.com>
2023-10-03 10:49:29 -0600

    small updates

--- skyjo_gym_game/game/skyjo_card.py
+++ skyjo_gym_game/game/skyjo_card.py
@@ -2,8 +2,7 @@ from termcolor import colored
 from skyjo_gym_game.game.utils import CARD_COLORS, COL_COUNT, ROW_COUNT, NA_VAL, UNK_VAL
 import numpy as np
 
-
-
+# In Utils:
 # CARD_COLORS = {-2:"blue", -1:"blue",
 #             0:"cyan",
 #             1: "green", 2:"green", 3:"green", 4:"green",
@@ -16,7 +15,7 @@ import numpy as np
 class SkyjoCard:
     def __init__(self, value) -> None:
         self.value = str(value)
-        self.trait = UNK_VAL  # UNK_VAL, NA_VAL, or value
+        self.trait = UNK_VAL
     
     def __call__(self):
         return self.trait
@@ -36,9 +35,6 @@ class SkyjoCard:
         
     def get_str(self):
         return str(self.trait)
-        # if self.trait == UNK_VAL or self.trait == NA_VAL:
-            # return self.state
-        # return f'{self.trait} ({self.color})'
     
     @staticmethod
     def print_cards(cards, fancy_print=True, print_array=True, return_only=False):
@@ -48,8 +44,6 @@ class SkyjoCard:
         '''
         if isinstance(cards, (int,str)):
             cards = [str(cards)]
-        # if isinstance(cards, str):
-            # cards = [cards]
         
         output = []
         cards = np.array(cards).flatten()
@@ -80,3 +74,54 @@ class SkyjoCard:
         if not return_only:
             print(res)
         return res
+    
+    
+    # VERSION 2 started
+    # @staticmethod
+    # def print_cards(cards, fancy_print=True, print_array=True, return_only=False):
+    #     ''' Print out card in a nice form
+    #     If it is not return_only, then it will print out the cards
+    #     Args:
+    #         card (str, int, list, or list of lists): will convert all to list of lists, where each sublist is a player
+            
+    #     '''
+    #     if isinstance(cards, (int,str)):
+    #         cards = [str(cards)]
+    #     if not isinstance(cards[0], list):
+    #         cards = [cards]
+            
+        
+    #     cards = [np.array(card_group).flatten() for card_group in cards]
+        
+    #     output = []
+        
+    #     if isinstance(cards[0][0], SkyjoCard):
+    #         cards = np.array([[card() for card in card_group] for card_group in cards])
+        
+        
+            
+        
+        
+    #     if print_array and len(cards) == (ROW_COUNT * COL_COUNT):
+    #         cards = np.reshape(cards, (ROW_COUNT, COL_COUNT))
+            
+    #         for row in cards:
+    #             row_out = []
+    #             for card in row:
+    #                 if fancy_print:
+    #                     row_out.append(colored(card, CARD_COLORS[card]))
+    #                 else:
+    #                     row_out.append(f'{card}')
+    #             output.append(" ".join(row_out))
+    #     else:
+    #         row_out = []
+    #         for card in cards:
+    #             if fancy_print:
+    #                 row_out.append(colored(card, CARD_COLORS[card]))
+    #             else:
+    #                 row_out.append(f'{card}')
+    #         output.append(", ".join(row_out))
+    #     res = "\n".join(output)
+    #     if not return_only:
+    #         print(res)
+    #     return res
